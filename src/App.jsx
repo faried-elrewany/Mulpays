043f5d4281ba7ad8014488bceadd519b2816c82c
faried-elrewany <fariedezz4@gmail.com>
2023-02-19 16:24:06 +0200

    added Hero Section Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,9 @@ const App = () => (
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth} `}>HERO SECTION</div>
+      <div className={`${styles.boxWidth} `}>
+        <Hero />
+      </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
